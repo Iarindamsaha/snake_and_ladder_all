4dc0f2a7b6a4c53c260b8e22bfcd6771de26e799
Arindam Saha <asaha15071998@gmail.com>
2023-02-12 21:06:32 +0530

    Adding Loop Feature Till 100

--- src/Main.java
+++ src/Main.java
@@ -6,44 +6,54 @@ public class Main {
         Random di = new Random();
 
         System.out.println("--- Welcome To Snake And Ladder Game ---");
+
         int start = 0;
         int position = 0;
+        int winning = 100;
+
         System.out.println("Starting Point : " + start);
 
-        int dice = di.nextInt(1,7);
-        System.out.println("The Dice Output is : " + dice);
 
         int check;
 
-        check = di.nextInt(3);
+        while (position <= winning) {
 
-        switch (check){
+            int dice = di.nextInt(1, 7);
+            System.out.println("The Dice Output is : " + dice);
+            check = di.nextInt(3);
 
-            case 0:{
+            switch (check) {
 
-                System.out.println("No Play || Staying in the same position");
-                break;
+                case 0: {
+
+                    System.out.println("No Play || Staying in the same position");
+                    break;
+                }
+
+                case 1: {
+
+                    position += dice;
+                    System.out.println(" Wow , it's a ladder your position is increased " + dice);
+                    break;
+                }
+
+                case 2: {
+
+
+                    position -=  dice;
+                    System.out.println(" Opps! it's a snake your position is decreased by " + dice);
+                    break;
+
+                }
+
+                default: {
+
+                    System.out.println("Wrong Wrong Wrong");
+                }
             }
+            System.out.println(position);
 
-            case 1:{
 
-                position += dice;
-                System.out.println(" Wow , it's a ladder your position is increased " + dice);
-                break;
-            }
-
-            case 2:{
-
-                position -= dice;
-                System.out.println(" Opps! it's a snake your position is decreased by " + dice);
-            }
-
-            default:{
-
-                System.out.println("Wrong Wrong Wrong");
-            }
         }
-
-
     }
 }
