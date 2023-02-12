eb6901b7ebfa908e2704bdacfcc0934ab3b09350
Arindam Saha <asaha15071998@gmail.com>
2023-02-12 20:19:54 +0530

    Starting With 0 Position

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("--- Welcome To Snake And Ladder Game ---");
+        int start = 0;
+        System.out.println(start);
+
     }
 }
