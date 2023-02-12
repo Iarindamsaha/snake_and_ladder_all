5543d08cc3958acfe3487b2d9ae8e22172b8a903
Arindam Saha <asaha15071998@gmail.com>
2023-02-12 22:39:54 +0530

    2 players Added

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
         System.out.println("--- Welcome To Snake And Ladder Game ---");
 
         int start = 0;
+        int player1 = 0;
+        int player2 = 1;
         int position = 0;
         int winning = 100;
         int diceRoll = 0;
@@ -20,61 +22,113 @@ public class Main {
         while (position <= winning) {
 
             diceRoll++;
-            int dice = di.nextInt(7)+1;
+            int players = di.nextInt(2);
+            int dice = di.nextInt(7) + 1;
             System.out.println("The Dice Output is : " + dice);
             check = di.nextInt(3);
 
-            switch (check) {
+            if (players == player1) {
 
-                case 0: {
+                switch (check) {
 
-                    System.out.println("No Play || Staying in the same position");
-                    break;
-                }
+                    case 0: {
 
-                case 1: {
+                        System.out.println("No Play || Staying in the same position");
+                        break;
+                    }
 
-                    position += dice;
-                    System.out.println(" Wow , it's a ladder your position is increased " + dice);
-                    if( position > winning){
-                        position -= dice;
+                    case 1: {
+
+                        position += dice;
+                        System.out.println(" Wow , it's a ladder your position is increased " + dice);
+
+                        if (position > winning) {
+                            position -= dice;
+
+                        } else if (position == winning) {
+                            System.out.println("----------------------");
+                            System.out.println("Player 1 Won The Game");
+                            System.out.println("Player Position" + position);
+                            System.out.println("Dice Played " + diceRoll + "Times");
+                            System.exit(0);
+                        }
+
+                        break;
+                    }
+
+                    case 2: {
+
+
+                        if ((position - dice) < start) {
+                            position = start;
+                        } else {
+                            position -= dice;
+                            System.out.println(" Opps! it's a snake your position is decreased by " + dice);
+                        }
+                        break;
 
                     }
 
-                    else if (position == winning) {
-                        System.out.println("You Won The Game");
-                        System.out.println("Player Position" + position);
-                        System.out.println("Dice Played " + diceRoll + "Times");
-                        System.exit(0);
-                    }
+                    default: {
 
-                    break;
+                        System.out.println("Wrong Wrong Wrong");
+                    }
                 }
 
-                case 2: {
+                System.out.println(position);
 
 
-                    if ((position - dice) < start) {
-                        position = start;
+            } else if (players == player2) {
+                switch (check) {
+
+                    case 0: {
+
+                        System.out.println("No Play || Staying in the same position");
+                        break;
                     }
-                    else {
-                        position -=  dice;
-                        System.out.println(" Opps! it's a snake your position is decreased by " + dice);
-                    }
-                    break;
 
+                    case 1: {
+
+                        position += dice;
+                        System.out.println(" Wow , it's a ladder your position is increased " + dice);
+
+                        if (position > winning) {
+                            position -= dice;
+
+                        } else if (position == winning) {
+                            System.out.println("----------------------");
+                            System.out.println("Player 2 Won The Game");
+                            System.out.println("Player  Position" + position);
+                            System.out.println("Dice Played " + diceRoll + "Times");
+                            System.exit(0);
+                        }
+
+                        break;
+                    }
+
+                    case 2: {
+
+
+                        if ((position - dice) < start) {
+                            position = start;
+                        } else {
+                            position -= dice;
+                            System.out.println(" Opps! it's a snake your position is decreased by " + dice);
+                        }
+                        break;
+
+                    }
+
+                    default: {
+
+                        System.out.println("Wrong Wrong Wrong");
+                    }
                 }
 
-                default: {
+                System.out.println(position);
 
-                    System.out.println("Wrong Wrong Wrong");
-                }
             }
 
-            System.out.println(position);
-
-
         }
-
     }
 }
