9123b83344e3cea9c91d971fd360f8b1ce738c70
Arindam Saha <asaha15071998@gmail.com>
2023-02-12 20:27:05 +0530

    Adding Dice To The Program

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
+        Random di = new Random();
+
         System.out.println("--- Welcome To Snake And Ladder Game ---");
         int start = 0;
-        System.out.println(start);
+        System.out.println("Starting Point : " + start);
+
+        int dice = di.nextInt(1,7);
+        System.out.println("The Dice Output is : " + dice);
+
+
 
     }
 }
