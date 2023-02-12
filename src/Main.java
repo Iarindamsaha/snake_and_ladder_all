360a61b4c9f12e10b7ed8cf3259902fea7f34400
Arindam Saha <asaha15071998@gmail.com>
2023-02-12 20:47:08 +0530

    Adding Play Feauture

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,42 @@ public class Main {
 
         System.out.println("--- Welcome To Snake And Ladder Game ---");
         int start = 0;
+        int position = 0;
         System.out.println("Starting Point : " + start);
 
         int dice = di.nextInt(1,7);
         System.out.println("The Dice Output is : " + dice);
 
+        int check;
+
+        check = di.nextInt(3);
+
+        switch (check){
+
+            case 0:{
+
+                System.out.println("No Play || Staying in the same position");
+                break;
+            }
+
+            case 1:{
+
+                position += dice;
+                System.out.println(" Wow , it's a ladder your position is increased " + dice);
+                break;
+            }
+
+            case 2:{
+
+                position -= dice;
+                System.out.println(" Opps! it's a snake your position is decreased by " + dice);
+            }
+
+            default:{
+
+                System.out.println("Wrong Wrong Wrong");
+            }
+        }
 
 
     }
